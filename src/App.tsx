b7f3e23d1ddab1747e26d89a5a6e8cbf5986c5c5
dinyardin <dinyardin@gmail.com>
2021-07-08 15:19:17 -0400

    Display country specific data on Infobox component

--- src/App.tsx
+++ src/App.tsx
@@ -16,10 +16,31 @@ interface Country {
   value: string;
 }
 
+interface CountryInfo {
+  cases: number;
+  recovered: number;
+  deaths: number;
+  todayCases: number;
+  todayRecovered: number;
+  todayDeaths: number;
+}
+
+const CountryInfo_initial = {
+  cases: 0,
+  recovered: 0,
+  deaths: 0,
+  todayCases: 0,
+  todayRecovered: 0,
+  todayDeaths: 0,
+};
+
 function App() {
   const url: string = "https://disease.sh/v3/covid-19/countries";
+  const url_worldwide: string = "https://disease.sh/v3/covid-19/all";
   const [countries, setCountries] = useState<Country[]>([]);
   const [country, setCountry] = useState<string>("worldwide");
+  const [countryInfo, setCountryInfo] =
+    useState<CountryInfo>(CountryInfo_initial);
 
   useEffect(() => {
     const getCountriesData = async () => {
@@ -35,12 +56,33 @@ function App() {
     };
 
     getCountriesData();
-  }, [countries]);
+  }, []);
 
   const onCountryChange = async (event: any) => {
     const countryCode = event.target.value;
     setCountry(countryCode);
+
+    // get country specific data
+    const _url: string =
+      countryCode === "worldwide"
+        ? url_worldwide
+        : url + `/${event.target.value}`;
+
+    await fetch(_url)
+      .then((response) => response.json())
+      .then((data) => {
+        setCountryInfo(data);
+      });
   };
+
+  // since select dropdown shows "worldwide" option by default, we need to show corresponding worldwide data on page load
+  useEffect(() => {
+    fetch(url_worldwide)
+      .then((response) => response.json())
+      .then((data) => {
+        setCountryInfo(data);
+      });
+  }, []);
 
   return (
     <div className="app">
@@ -67,20 +109,20 @@ function App() {
         <div className="app__stats">
           <InfoBox
             title="Coronavirus Cases"
-            cases={212321}
-            total={123232}
+            cases={countryInfo.todayCases}
+            total={countryInfo.cases}
           ></InfoBox>
 
           <InfoBox
-            title="Coronavirus Cases"
-            cases={212321}
-            total={123232}
+            title="Recovered"
+            cases={countryInfo.todayRecovered}
+            total={countryInfo.recovered}
           ></InfoBox>
 
           <InfoBox
-            title="Coronavirus Cases"
-            cases={212321}
-            total={123232}
+            title="Deaths"
+            cases={countryInfo.todayDeaths}
+            total={countryInfo.deaths}
           ></InfoBox>
         </div>
 
